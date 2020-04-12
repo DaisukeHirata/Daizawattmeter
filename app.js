11d1f51a776ea14e8d46a304aad31e8a19666861
Daisuke Hirata <daisukihirata@gmail.com>
2020-04-12 12:25:01 +0900

    add nature remo

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ app.use(express.static(path.join(__dirname, 'public')));
  
 app.get('/', currentAmpere.index);
 app.get('/currentAmpere', currentAmpere.findLast);
+app.get('/currentNatureRemo', currentAmpere.findLastNatureRemo);
 app.get('/lastNAmpere', currentAmpere.findLastN);
 app.get('/update', currentAmpere.update);
+app.get('/updateNatureRemo', currentAmpere.updateNatureRemo);
 
 app.listen(app.get('port'));
 console.log('Listening on port 3000...');
--- routes/currentAmpere.js
+++ routes/currentAmpere.js
@@ -9,12 +9,21 @@ var Schema = mongoose.Schema;
 var CurrentAmpere = new Schema({
     ampere: Number 
 }, { collection: 'currentAmpere' });
+
+// Defaultのスキーマから新しいスキーマを定義
+var NatureRemo = new Schema({
+    humidity: Number,
+    illuminance: Number,
+    temperature: Number
+}, { collection: 'natureRemo' });
  
 // モデル化。model('[登録名]', '定義したスキーマクラス')
 mongoose.model('CurrentAmpere', CurrentAmpere);
- 
+mongoose.model('NatureRemo', NatureRemo);
+
 var CurrentAmpere;
- 
+var NatureRemo;
+
 // mongodb://[hostname]/[dbname]
 mongoose.connect('mongodb://localhost/dhmongo');
  
@@ -25,6 +34,7 @@ db.once('open', function() {
   console.log("Connected to 'dhmongo' database");
   // 定義したときの登録名で呼び出し
   CurrentAmpere = mongoose.model('CurrentAmpere');
+  NatureRemo = mongoose.model('NatureRemo');
 });
 
 exports.index = function(req, res){
@@ -33,11 +43,26 @@ exports.index = function(req, res){
  
 exports.findLast = function(req, res) {
   console.log('Getting CurrentAmpere');
- 
+
   CurrentAmpere.findOne().sort({'$natural':-1}).exec(function(err, results) {
     if (!err) {
       console.log('Success: Getting currentAmpere');
       res.json(results.ampere/10.0);
+    } else {
+      res.send({'error': 'An error has occurred'});
+    }
+  });
+};
+
+exports.findLastNatureRemo = function(req, res) {
+  console.log('Getting NatureRemo');
+
+  NatureRemo.findOne().sort({'$natural':-1}).exec(function(err, results) {
+    if (!err) {
+      console.log('Success: Getting NatureRemo');
+      res.json({humidity: results.humidity,
+                illuminance: results.illuminance,
+                temperature: results.temperature/10.0});
     } else {
       res.send({'error': 'An error has occurred'});
     }
@@ -98,6 +123,51 @@ exports.update = function(req, res) {
   });
   res.write('\n');
  
+  req.on("close", function() {
+    stream.destroy();
+    console.log("Client disconnected.");
+  });
+};
+
+exports.updateNatureRemo = function(req, res) {
+  req.socket.setTimeout(Number.MAX_SAFE_INTEGER);
+
+  var stream;
+  NatureRemo.findOne().sort({'$natural':-1}).exec(function(err, item){
+    if(err){
+      console.log(err);
+    }
+    stream = NatureRemo.find().gt('_id', item._id).sort({'$natural': 1}).tailable().stream();
+    var messageCount = 0;
+
+    stream.on("error", function(err) {
+      console.log("Mongo Error: " + err);
+    });
+
+    stream.on('data', function(doc){
+      messageCount++;
+      //console.log(messageCount);
+      res.write('id: ' + messageCount + '\n');
+      var msg = JSON.stringify({
+        humidity: doc.humidity,
+        illuminance: doc.illuminance,
+        temperature: doc.temperature/10.0
+      });
+      res.write("data: "+msg+"\n\n");
+    });
+
+    stream.on('close', function(){
+      console.log("Mongo closed");
+    });
+  });
+
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  res.write('\n');
+
   req.on("close", function() {
     stream.destroy();
     console.log("Client disconnected.");
